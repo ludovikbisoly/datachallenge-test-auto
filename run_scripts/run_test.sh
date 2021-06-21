7d99d3ffe7b3c6b2f56d076ed0f5396c25fabc57
ludo <ludovik.bisoly@ifsalpha.com>
2021-06-21 15:26:02 +0200

    hardcode apikey

--- run_scripts/run_test.sh
+++ run_scripts/run_test.sh
@@ -7,8 +7,9 @@
 TEST_PROJECT_NAME=datachallenge-test-auto
 
 HOME_DIRECTORY=~/workspace
-#source ${HOME_DIRECTORY}/${TEST_PROJECT_NAME}/ci_config/project_settings.ini
-export TEST_APIKEY="8af2e205-6a35-4819-8fb2-452c8f3c5604"
+
+source ${HOME_DIRECTORY}/${TEST_PROJECT_NAME}/ci_config/project_settings.ini
+#export TEST_APIKEY="8af2e205-6a35-4819-8fb2-452c8f3c5604"
 
 #############################################################################
 #                        KATALON TESTSUITES EXECUTION                       #
@@ -20,5 +21,5 @@ echo "----------------"
 echo "test echo executionProfile"
 echo "${executionProfile}"
 echo "----------------"
-bash katalon-execute.sh -executionProfile="default" -browserType="Chrome" -retry=0 -testSuitePath="Test Suites/API/datachallenge-backend" -apiKey=${TEST_APIKEY}
+bash katalon-execute.sh -executionProfile="default" -browserType="Chrome" -retry=0 -testSuitePath="Test Suites/API/datachallenge-backend" -apiKey="8af2e205-6a35-4819-8fb2-452c8f3c5604"
 #-g_x_api_key=${apiKeyValue} -g_user_pool_id=${userPoolId} -g_app_id=${clientId} -g_admin_password=${pwd_admin} -g_admin_username=${username_admin}
