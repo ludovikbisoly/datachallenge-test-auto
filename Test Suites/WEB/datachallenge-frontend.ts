d2ed5c7c098ac93e8754cf5798a8944574eb4af1
ludo <ludovik.bisoly@ifsalpha.com>
2021-06-21 15:59:30 +0200

    edit test suit front end

--- Test Suites/WEB/datachallenge-frontend.ts
+++ Test Suites/WEB/datachallenge-frontend.ts
@@ -11,4 +11,34 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>b98bae41-6f67-4adb-822c-e208406469a7</testSuiteGuid>
+   <testCaseLink>
+      <guid>d6caaded-8b4d-4cc2-b98b-8f7c3b5cebc8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/WEB/TESS_Home_Page</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6d5fed6e-cae3-487e-8c66-887d615b5294</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WEB/TESS_Log_Out</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8578bc22-696d-4301-8afd-56bd7e62fa57</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/WEB/TESS_Log_In_Cas_Non_Passant</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1d2559d8-984c-489b-beb5-c81613728ebd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WEB/TESS_Lexique</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8fb09d18-06f0-4ad7-9e5a-fe0056b32123</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WEB/TESS_Login_In_Cas_Passant</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
